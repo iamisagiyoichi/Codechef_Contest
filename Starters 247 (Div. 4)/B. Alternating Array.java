fc22456a1d1c86eb228b6bb861a302ca5a1d7398
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-15 23:38:13 +0530

    Add B. Alternating Array solution

---
+++ Starters 247 (Div. 4)/B. Alternating Array.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class FastReader {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+
+        int[] nextIntArray(int n) throws IOException {
+            int[] a = new int[n];
+            int b = 0;
+            while (b < n) {
+                a[b] = nextInt();
+                b++;
+            }
+            return a;
+        }
+
+        long[] nextLongArray(int n) throws IOException {
+            long[] a = new long[n];
+            int b = 0;
+            while (b < n) {
+                a[b] = nextLong();
+                b++;
+            }
+            return a;
+        }
+    }
+
+    static FastReader sc = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static int count(int[] a, int b) {
+        int c = 0;
+        int d = 0;
+
+        while (d < a.length) {
+            if ((a[d] & 1) != b) {
+                c++;
+            }
+            b ^= 1;
+            d++;
+        }
+
+        return c;
+    }
+
+    public static void solve() throws Exception {
+        int a = sc.nextInt();
+        int[] b = sc.nextIntArray(a);
+
+        int c = count(b, 1);
+        int d = count(b, 0);
+
+        out.println(Math.min(c, d));
+    }
+
+    public static void main(String[] args) throws Exception {
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            solve();
+        }
+
+        out.flush();
+    }
+}
