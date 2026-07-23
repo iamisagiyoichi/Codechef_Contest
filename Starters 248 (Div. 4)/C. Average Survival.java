f38cb0c6f9cedbedad4ca99c9478412e2796163d
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-23 18:41:15 +0530

    Update C. Average Survival.java

--- Starters 248 (Div. 4)/C. Average Survival.java
+++ Starters 248 (Div. 4)/C. Average Survival.java
@@ -1,1 +1,57 @@
+import java.io.*;
+import java.util.*;
 
+public class Main {
+
+    static class FastReader {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    static FastReader sc = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void solve() throws Exception {
+        int n = sc.nextInt();
+
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+        while (c < n) {
+            int d = sc.nextInt();
+
+            if (d > a) {
+                a = d;
+                b = 1;
+            } else if (d == a) {
+                b++;
+            }
+
+            c++;
+        }
+
+        out.println(b);
+    }
+
+    public static void main(String[] args) throws Exception {
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            solve();
+        }
+
+        out.flush();
+    }
+}
