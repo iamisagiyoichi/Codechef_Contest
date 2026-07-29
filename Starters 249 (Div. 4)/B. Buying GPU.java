e80d30c2c3bb088f2e58287e4deec60aea75e762
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-29 23:58:52 +0530

    Update B. Buying GPU.java

--- Starters 249 (Div. 4)/B. Buying GPU.java
+++ Starters 249 (Div. 4)/B. Buying GPU.java
@@ -1,1 +1,47 @@
+import java.io.*;
+import java.util.*;
 
+public class Main {
+
+    static class FastReader {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    static FastReader sc = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void solve() throws Exception {
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        if (z <= y) {
+            out.println(-1);
+            return;
+        }
+        int d = z - y;
+        int ans = (x + d - 1) / d;
+        out.println(ans);
+    }
+
+    public static void main(String[] args) throws Exception {
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            solve();
+        }
+
+        out.flush();
+    }
+}
